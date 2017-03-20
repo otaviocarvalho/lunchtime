93f217310353c4d93085756a06ba7d88242d1c88
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-03-19 22:59:08 -0300

    MVP1 for lunch app

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = state;
+
+        this.sort = this.sort.bind(this);
+    }
+
+    render() {
+        return (
+            <View style={ styles.container }>
+                <Text style={ styles.text }>
+                    { this.state.restaurant }
+                </Text>
+                <Button
+                  title="E hoje?"
+                  onPress={this.sort}
+                  accessibilityLabel="Choose a restaurant for today's lunch"
+                />
+            </View>
+        );
+    }
+
+    sort(event) {
+        var getRandomRestaurant = (restaurants) => {
+            return restaurants[ Math.floor( Math.random() * restaurants.length) ];
+        };
+
+        this.setState(prevState => ({
+            'restaurant': getRandomRestaurant(prevState.restaurants)
+        }));
+    }
+
+}
+
+const state = {
+    'restaurants': [
+        'Silvão',
+        '40inha',
+        'Bingo do 50',
+        'Onze',
+        'Tiozinho',
+        'Sabor Família',
+        'Palatu\'s'
+    ]
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: 'white',
+        fontSize: 60,
+        padding: 20,
+        fontWeight: 'bold',
+    }
+});
